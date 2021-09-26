a5541140276e89aea8e640e7c88f38968970723b
Ella Madalinski <ella.madalinski24@d155.org>
2021-09-26 11:59:02 -0500

    initial and final commit

--- NotesApp_SegmentedController/ViewController.swift
+++ NotesApp_SegmentedController/ViewController.swift
@@ -14,6 +14,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        
+        var select = sender.selectedSegmentIndex
+        
+        switch select {
+        case 0:
+            print("you selected pony")
+        case 1:
+            print("you selected dog")
+        case 2:
+            print("you selected bear")
+        default:
+            print("you didn't select anything")
+        }
+        
+//        if select == 0 {
+//            print("you selected pony")
+//        }
+//        else if select == 1 {
+//            print("you selected dog")
+//        }
+//        else {
+//            print("you selected bear")
+//        }
+        
+    }
+    
 
 }
 
